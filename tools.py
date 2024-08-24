f680bdc47a99e4e5ff180225406bc3b7a117f0ef
Michael Yartys <mail@yartys.no>
2024-08-24 22:19:48 +0200

    tools: add class to ease retrying failed municipalities
    Add a class for getting tertiary domains from municipalities. The class
    has built in functionality to retry failed municipalities, as this is
    something failures have been observed previously.
    
    Signed-off-by: Michael Yartys <mail@yartys.no>

--- tools.py
+++ tools.py
@@ -1,26 +1,113 @@
-from playwright.sync_api import TimeoutError as PlaywrightTimeoutError
+from playwright.sync_api import sync_playwright, TimeoutError as PlaywrightTimeoutError
 import time
 from io import BytesIO
 import pycurl
-from tenacity import retry, retry_if_exception_type, retry_if_result, stop_after_attempt
+from tenacity import retry, retry_if_exception_type, retry_if_result, stop_after_attempt, RetryError
+from urllib.parse import urlparse
 
 
-@retry(retry=retry_if_exception_type(PlaywrightTimeoutError), stop=stop_after_attempt(3))
-def load_page(context, url):
-    page = context.new_page()
-    response_urls = []
-    page.on('response', lambda response: response_urls.append(response.url))
-    try:
-        page.goto(url, wait_until='load', timeout=25000)
-    except PlaywrightTimeoutError:
-        page.close()
-        raise
-    else:
-        # Wait 5 seconds for any additional resources to load before closing the page.
-        time.sleep(5)
-        page.close()
+class TertiaryDomains:
+    def __init__(self, municipalities, urls):
+        self.municipalities = municipalities
+        self.urls = urls
 
-    return response_urls
+        p = sync_playwright().start()
+        browser = p.firefox.launch()
+        context = browser.new_context(user_agent='Mozilla/5.0 (X11; Linux x86_64; rv:127.0) Gecko/20100101'
+                                                 ' Firefox/127.0', ignore_https_errors=True)
+
+        self.results = {}
+        self.failed_municipalities = []
+        self.failed_urls = []
+        for municipality, url in zip(municipalities, urls):
+            try:
+                print(f'Getting tertiary domains of {municipality}...', end='')
+                responses = self._load_page(context, 'https://' + url)
+            except RetryError:
+                self.results[municipality] = None
+                self.failed_municipalities.append(municipality)
+                self.failed_urls.append(url)
+                print('Failed!')
+            else:
+                if 'www.' not in url:
+                    url = 'www.' + url
+
+                netlocs = []
+                for response in responses:
+                    netloc = urlparse(response).netloc
+                    # Avoid appending the netloc of municipality to tertiary netloc list.
+                    if netloc and netloc not in url:
+                        netlocs.append(netloc)
+
+                self.results[municipality] = sorted(set(netlocs))
+                print('Finished!')
+
+        p.stop()
+
+    @retry(retry=retry_if_exception_type(PlaywrightTimeoutError), stop=stop_after_attempt(3))
+    def _load_page(self, context, url):
+        page = context.new_page()
+        response_urls = []
+        page.on('response', lambda response: response_urls.append(response.url))
+        try:
+            page.goto(url, wait_until='load', timeout=25000)
+        except PlaywrightTimeoutError:
+            page.close()
+            raise
+        else:
+            # Wait 5 seconds for any additional resources to load before closing the page.
+            time.sleep(5)
+            page.close()
+
+        return response_urls
+
+    def retry_failed(self, retries: int, delay):
+        if self.failed_municipalities:
+            for retry in range(retries):
+                if self.failed_municipalities:
+                    print(f'Retry {retry + 1}')
+                    print('Failed municipalities:')
+                    for municipality in self.failed_municipalities:
+                        print(municipality)
+
+                    print(f'Waiting {delay} seconds.')
+                    time.sleep(delay)
+
+                    p = sync_playwright().start()
+                    browser = p.firefox.launch()
+                    context = browser.new_context(user_agent='Mozilla/5.0 (X11; Linux x86_64; rv:127.0) Gecko/20100101'
+                                                             ' Firefox/127.0', ignore_https_errors=True)
+
+                    for municipality, url in zip(self.failed_municipalities, self.failed_urls):
+                        try:
+                            print(f'Getting tertiary domains of {municipality}...', end='')
+                            responses = self._load_page(context, url)
+                        except RetryError:
+                            print('Failed!')
+                        else:
+                            self.failed_municipalities.remove(municipality)
+                            self.failed_urls.remove(url)
+
+                            if 'www.' not in url:
+                                url = 'www.' + url
+
+                            netlocs = []
+                            for response in responses:
+                                netloc = urlparse(response).netloc
+                                # Avoid appending the netloc of municipality to tertiary netloc list.
+                                if netloc and netloc not in url:
+                                    netlocs.append(netloc)
+
+                            self.results[municipality] = sorted(set(netlocs))
+
+                            print('Finished!')
+
+                    p.stop()
+
+                else:
+                    print('No failed municipalities!')
+        else:
+            print('No failed municipalities!')
 
 
 def is_timeout(result):
