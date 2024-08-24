efa74feabcf5061dddc79ef6a5fcde2023a5a40f
Michael Yartys <mail@yartys.no>
2024-08-24 22:22:01 +0200

    loop_municipalities: use new class
    Use the new class for getting tertiary domains, and retry any failed
    municipalities up to 3 times with a delay of 1 hour per retry.
    
    Signed-off-by: Michael Yartys <mail@yartys.no>

--- loop_municipalities.py
+++ loop_municipalities.py
@@ -1,8 +1,6 @@
 import numpy as np
-from tools import load_page, curl_ipv6_request
-from urllib.parse import urlparse
+from tools import curl_ipv6_request, TertiaryDomains
 import json
-from playwright.sync_api import sync_playwright
 
 municipalities, urls = np.loadtxt('/input/kommuner.csv',
                                   skiprows=1,
@@ -11,12 +9,10 @@ municipalities, urls = np.loadtxt('/input/kommuner.csv',
                                   dtype='str',
                                   unpack=True)
 
-p = sync_playwright().start()
-browser = p.firefox.launch()
-context = browser.new_context(user_agent='Mozilla/5.0 (X11; Linux x86_64; rv:127.0) Gecko/20100101 Firefox/127.0',
-                              ignore_https_errors=True)
+municipalities = list(municipalities)
+urls = list(urls)
 
-data = {}
+own_domains = {}
 for municipality, url in zip(municipalities, urls):
     print(f'Getting {municipality}...', end='')
 
@@ -28,24 +24,20 @@ for municipality, url in zip(municipalities, urls):
         own.append(['www.' + url, curl_ipv6_request('https://www.' + url)])
         own.append([url, curl_ipv6_request('https://' + url)])
 
-    responses = load_page(context, 'https://' + url)
+    own_domains[municipality] = own
 
-    netlocs = []
-    for response in responses:
-        netloc = urlparse(response).netloc
-        # Avoid appending the netloc of municipality to tertiary netloc list.
-        if netloc and netloc not in own[0][0]:
-            netlocs.append(netloc)
-
-    unique_netlocs = sorted(set(netlocs))
-    data[str(municipality)] = dict(own=own, tertiary=unique_netlocs)
     print('Finished!')
 
-p.stop()
+tertiary_domains = TertiaryDomains(municipalities, urls)
+tertiary_domains.retry_failed(3, 3600)
+
+data = {}
+for municipality in municipalities:
+    data[municipality] = {'own': own_domains[municipality], 'tertiary': tertiary_domains.results[municipality]}
 
 tertiary = []
-for netloc_list in data.values():
-    tertiary += netloc_list['tertiary']
+for netloc_list in tertiary_domains.results.values():
+    tertiary += netloc_list
 
 unique_tertiary, counts = np.unique(np.array(tertiary), return_counts=True)
 sorted_index = np.argsort(unique_tertiary)
